8c606c6f6ccc67c7be1cb08acfff4446c729178c
Jorge Venegas <brangelina01@gmail.com>
2020-10-01 15:21:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 
 def countdown(seconds)
-  while seconds < 11
-    puts "#{number} SECOND(S)!"
+  while seconds < 10 
+    puts "#{seconds} SECOND(S)!"
   seconds -= 1 
   end 
    return "HAPPY NEW YEAR!"
